fe5c498e7ee6e9a016e029f45bb35e668a059f22
briansrt <brianstevenriofriotrochez@hotmail.com>
2024-10-21 19:29:16 -0500

    Arreglando verificacion de users

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,12 @@
 import './styles/Form.css'
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const goTo = useNavigate();
+  
   const validateUser = async (event)=>{
     event.preventDefault();
     try {
